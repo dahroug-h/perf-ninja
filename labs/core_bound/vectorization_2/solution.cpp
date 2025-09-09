4eb4366480869a194f360db1b20461e3d917b411
CrazyRoka <rokarostuk@gmail.com>
2025-09-09 23:12:16 +0100

    Solve loop vectorization 2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,16 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
+  uint32_t acc = 0;
   for (auto value : blob) {
     acc += value;
-    acc += acc < value; // add carry
+    // acc += acc < value; // add carry
   }
-  return acc;
+  
+  while (acc > 0xFFFF) {
+    uint32_t lower = acc & 0xFFFF;
+    uint32_t higher = acc >> 16;
+    acc = higher + lower;
+  }
+  return acc & 0xFFFF;
 }
