e2b20214cef872293f46eed64fa2c2a52360fba2
Rostyslav Toch <rokarostuk@gmail.com>
2025-09-14 17:46:14 +0100

    Solve virtual call mispredict

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -1,6 +1,20 @@
 #include "solution.h"
 
+#include <algorithm>
 #include <random>
+
+int getClassId(const BaseClass* obj) {
+    if (dynamic_cast<ClassA const*>(obj) != nullptr) {
+        return 0;
+    }
+    if (dynamic_cast<ClassB const*>(obj) != nullptr) {
+        return 1;
+    }
+    if (dynamic_cast<ClassC const*>(obj) != nullptr) {
+        return 2;
+    }
+    return 3;
+}
 
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
@@ -16,6 +30,10 @@ void generateObjects(InstanceArray& array) {
             array.push_back(std::make_unique<ClassC>());
         }
     }
+
+    std::sort(array.begin(), array.end(), [](const std::unique_ptr<BaseClass>& left, const std::unique_ptr<BaseClass>& right) {
+        return getClassId(left.get()) < getClassId(right.get());
+    });
 }
 
 // Invoke the `handle` method on all instances in `output`
