2967fdb1baf3c1a1c17fca972f8534502626ddc7
Rostyslav Toch <rokarostuk@gmail.com>
2025-09-14 17:24:24 +0100

    Solution for conditional store

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -6,8 +6,9 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
   for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
+    output[count] = item;
+    if (((lower <= item.first) && (item.first <= upper))) {
+      count = count + 1;
     }
   }
   return count;
