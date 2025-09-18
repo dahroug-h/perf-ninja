d4b119dfa10a1b04d8d743a7624e337750170e91
snzhou <zhou-sn@qq.com>
2025-09-18 11:12:12 +0800

    change the number of iterations

--- labs/misc/warmup/bench.cpp
+++ labs/misc/warmup/bench.cpp
@@ -20,7 +20,7 @@ static void bench1(benchmark::State &state) {
 }
 
 // Register the function as a benchmark
-BENCHMARK(bench1);
+BENCHMARK(bench1)->Iterations(1000000);
 
 // Run the benchmark
 BENCHMARK_MAIN();
