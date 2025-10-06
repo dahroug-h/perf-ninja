3fba0248ffbf10e67402f45cf5ae6dd54c905f0b
YukunJ <yukunj@andrew.cmu.edu>
2025-10-05 20:17:25 -0400

    add stuff forgot to commit for prefetch lab

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -9,13 +9,25 @@ static int getSumOfDigits(int n) {
   return sum;
 }
 
+constexpr int lookahead = 8;
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
-
-  for (int val : lookups) {
-    if (hash_map->find(val))
-      result += getSumOfDigits(val);
+  if (lookups.empty()) {
+    return result;
   }
 
+  for (int i = 0; i < lookups.size() - lookahead; i++) {
+    int val = lookups[i];
+    hash_map->prefetch(lookups[i+lookahead]);
+    if (hash_map->find(val)) {
+      result += getSumOfDigits(val);
+    }
+  }
+  for (int i = lookups.size() - lookahead; i < lookups.size(); i++) {
+    int val = lookups[i];
+    if (hash_map->find(val)) {
+      result += getSumOfDigits(val);
+    }
+  }
   return result;
 }
