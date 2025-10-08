74fa619663db68b5399b6248984eef41e813284b
YukunJ <yukunj@andrew.cmu.edu>
2025-10-07 22:50:08 -0400

    lab: loop tiling

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,9 +3,14 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  for (int i = 0; i < size; i += 8) {
+    for (int j = 0; j < size; j += 8) {
+      for (int m = i; m < std::min(i + 8, size); m++) {
+        for (int n = j; n < std::min(j + 8, size); n++) {
+          out[m][n] = in[n][m];
+        }
+      }
+      // out[i][j] = in[j][i];
     }
   }
   return out[0][size - 1];
