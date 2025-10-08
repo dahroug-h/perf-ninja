33d135bd4c35d2e365eb22e525a82bfe51f2537b
YukunJ <yukunj@andrew.cmu.edu>
2025-10-07 22:56:01 -0400

    lab: loop tiling using cache line size

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,7 +1,15 @@
 #include "solution.hpp"
 #include <algorithm>
+#ifdef __cpp_lib_hardware_interference_size
+    using std::hardware_constructive_interference_size;
+    using std::hardware_destructive_interference_size;
+#else
+    // 64 bytes on x86-64 │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ __cacheline_aligned │ ...
+    constexpr std::size_t hardware_constructive_interference_size = 64;
+    constexpr std::size_t hardware_destructive_interference_size = 64;
+#endif
 
-static constexpr int cache_line = std::hardware_constructive_interference_size;
+static constexpr int cache_line = hardware_constructive_interference_size;
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
   int count_per_cache_line = cache_line / sizeof(double);
