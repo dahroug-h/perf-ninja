d14995654f475f66e69382cd7d460ab978161617
YukunJ <yukunj@andrew.cmu.edu>
2025-10-07 22:54:19 -0400

    lab: loop tiling using cache line size

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,16 +1,17 @@
 #include "solution.hpp"
 #include <algorithm>
 
+static constexpr int cache_line = std::hardware_constructive_interference_size;
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i += 8) {
-    for (int j = 0; j < size; j += 8) {
-      for (int m = i; m < std::min(i + 8, size); m++) {
-        for (int n = j; n < std::min(j + 8, size); n++) {
+  int count_per_cache_line = cache_line / sizeof(double);
+  for (int i = 0; i < size; i += count_per_cache_line) {
+    for (int j = 0; j < size; j += count_per_cache_line) {
+      for (int m = i; m < std::min(i + count_per_cache_line, size); m++) {
+        for (int n = j; n < std::min(j + count_per_cache_line, size); n++) {
           out[m][n] = in[n][m];
         }
       }
-      // out[i][j] = in[j][i];
     }
   }
   return out[0][size - 1];
