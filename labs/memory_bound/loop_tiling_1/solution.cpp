41c087bfb098b744180823cb635833f43b2781ec
Peter Moroz <ptr.moroz@gmail.com>
2025-10-24 09:14:47 +0300

    add: solution of loop tiling lab

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,9 +3,14 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  constexpr int TILE_SIZE = 16;
+  for (int ii = 0; ii < size; ii += TILE_SIZE) {
+    for (int jj = 0; jj < size; jj += TILE_SIZE) {
+      for (int i = ii; i < std::min(size, ii + TILE_SIZE); i++) {
+        for (int j = jj; j < std::min(size, jj + TILE_SIZE); j++) {
+          out[i][j] = in[j][i];
+        }
+      }
     }
   }
   return out[0][size - 1];
