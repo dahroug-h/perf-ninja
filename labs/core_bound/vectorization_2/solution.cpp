2254d44b74b50f52bd49a23f46e15be4c9585868
YukunJ <yukunj.cs@gmail.com>
2025-10-30 00:32:22 +0000

    vectorization lab 2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -5,5 +5,7 @@ uint16_t checksum(const Blob &blob) {
   for (auto value : blob) {
     acc += value;
   }
-  return (acc & 0xfffff) + (acc >> 16);
+  acc = (acc & 0xffff) + (acc >> 16);
+  acc = (acc & 0xffff) + (acc >> 16); // in case the final add overflow
+  return acc;
 }
