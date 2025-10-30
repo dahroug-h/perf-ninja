724bc5ed1fbdc3889dc42c349f8498115049d0bf
Vikas Kurapati <vikas.kurapati@tum.de>
2025-10-30 10:51:11 +0100

    Attempt at passing windows solution by making everything the same datatype

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,11 @@ constexpr int maxRandom = 100;
 // FIXME: this data structure can be reduced in size
 struct S {
   float d;
-  unsigned long long l: 16;
+  unsigned int l: 16;
   unsigned int i: 8;
-  unsigned short s: 7;
-  bool b: 1;
-
+  unsigned int s: 7;
+  unsigned int b: 1;
+  
   bool operator<(const S &s) const { return this->i < s.i; }
 };
 
