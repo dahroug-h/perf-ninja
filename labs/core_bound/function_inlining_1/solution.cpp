020f8392a1b23ec0b89f2f8be849967a33a7081c
YukunJ <yukunj.cs@gmail.com>
2025-10-31 22:17:17 +0000

    inline func

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <stdlib.h>
 
-static int compare(const void *lhs, const void *rhs) {
+static inline int compare(const void *lhs, const void *rhs) {
   auto &a = *reinterpret_cast<const S *>(lhs);
   auto &b = *reinterpret_cast<const S *>(rhs);
 
@@ -23,5 +23,11 @@ static int compare(const void *lhs, const void *rhs) {
 }
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  // qsort(arr.data(), arr.size(), sizeof(S), compare);
+  std::sort(arr.begin(), arr.end(),
+    [](const S &lhs, const S &rhs) -> bool {
+      if (lhs.key1 != rhs.key1) return lhs.key1 < rhs.key1;
+      return lhs.key2 < rhs.key2;
+    }
+  );
 }
