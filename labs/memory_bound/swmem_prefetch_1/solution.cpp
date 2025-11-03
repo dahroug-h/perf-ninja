e8b5b313b2b0dac784d6ea02df76d97bc18caf6c
Vikas Kurapati <vikas.kurapati@tum.de>
2025-11-03 16:25:28 +0100

    software prefetching

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -9,12 +9,23 @@ static int getSumOfDigits(int n) {
   return sum;
 }
 
+constexpr int look_ahead = 16;
+
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
-  for (int val : lookups) {
-    if (hash_map->find(val))
+  for (size_t i = 0; i < lookups.size()-look_ahead; ++i) {
+    int val = lookups[i];
+    if (hash_map->find(val)) //cache misses happen here
       result += getSumOfDigits(val);
+
+    hash_map->prefetchForVal(lookups[i + look_ahead]);
+  }
+
+  for (size_t i = lookups.size()-look_ahead; i < lookups.size(); ++i) {
+      int val = lookups[i];
+      if (hash_map->find(val))
+        result += getSumOfDigits(val);
   }
 
   return result;
