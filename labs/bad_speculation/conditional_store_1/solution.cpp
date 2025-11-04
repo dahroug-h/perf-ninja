9aecec1e2155fecaf3cc75f08f07a1b371c1964b
Vikas Kurapati <vikas.kurapati@tum.de>
2025-11-04 11:18:03 +0100

    Revert "Remove conditional branches and replace with ternary operators"
    This reverts commit 7570493da93899840c28e1b6c6a1219110bddda4.

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -6,12 +6,9 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
   for (const auto item : input) {
-
-    auto condition = (lower <= item.first) && (item.first <= upper);
-
-    output[count] = condition ? item : output[count];
-    count = condition ? count + 1 : count;
+    if ((lower <= item.first) && (item.first <= upper)) {
+      output[count++] = item;
+    }
   }
-
   return count;
 }
