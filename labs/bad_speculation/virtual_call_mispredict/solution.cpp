7ad1229fb4e2afb27bd5a585c6978cf717f3983a
Vikas Kurapati <vikas.kurapati@tum.de>
2025-11-04 13:17:13 +0100

    Order does not matter solution

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -6,21 +6,31 @@ void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    InstanceArray arrayA;
+    InstanceArray arrayB;
+    InstanceArray arrayC;
+
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
 
-        switch (value) {
-            case 0:
-                array.push_back(std::make_unique<ClassA>());
-                break;
-            case 1:
-                array.push_back(std::make_unique<ClassB>());
-                break;
-            default:
-                array.push_back(std::make_unique<ClassC>());
-                break;
+        if (value==0) {
+            arrayA.push_back(std::make_unique<ClassA>());
+        } else if (value==1) {
+            arrayB.push_back(std::make_unique<ClassB>());
+        } else {
+            arrayC.push_back(std::make_unique<ClassC>());
         }
     }
+
+    array.insert(array.end(),
+                 std::make_move_iterator(arrayA.begin()),
+                 std::make_move_iterator(arrayA.end()));
+    array.insert(array.end(),
+                 std::make_move_iterator(arrayB.begin()),
+                 std::make_move_iterator(arrayB.end()));
+    array.insert(array.end(),
+                 std::make_move_iterator(arrayC.begin()),
+                 std::make_move_iterator(arrayC.end()));
 }
 
 // Invoke the `handle` method on all instances in `output`
