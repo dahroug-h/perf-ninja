7626920b24241ea1b3bbf412adb9c3c9a8630438
Vikas Kurapati <vikas.kurapati@tum.de>
2025-11-05 16:35:04 +0100

    modify the return function

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -23,5 +23,8 @@ static int compare(const void *lhs, const void *rhs) {
 }
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  // std::sort(arr.data(), arr.data() + arr.size(), compare);
+  std::sort(arr.begin(), arr.end(), [](const S &a, const S &b) {
+    return a.key1 < b.key1 || (a.key1 == b.key1) && (a.key2 < b.key2);
+  });
 }
