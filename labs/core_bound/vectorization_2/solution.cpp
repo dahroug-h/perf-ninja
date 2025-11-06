4f38e04b247257905b7e8c770a28eff0accba075
Vikas Kurapati <vikas.kurapati@tum.de>
2025-11-06 10:58:10 +0100

    accumulator

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,17 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
+  uint32_t acc = 0;
   for (auto value : blob) {
     acc += value;
-    acc += acc < value; // add carry
   }
-  return acc;
+    auto high = acc >> 16;
+    auto low = acc & 0xFFFFu;
+    acc = high + low;
+
+    high = acc >> 16;
+    low = acc & 0xFFFFu;
+    acc = high + low;
+
+  return static_cast<uint16_t>(acc);
 }
