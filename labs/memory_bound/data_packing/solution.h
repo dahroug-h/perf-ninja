6353d513ba683a5f58cd1823220e86d95df82945
John Tan <john.v.tan@gmail.com>
2025-11-08 09:44:04 -0500

    data_packing: reduced sizeof(S) to 8 bytes

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -1,17 +1,17 @@
 #include <vector>
+#include <stdint.h>
 
 // Assume those constants never change
 constexpr int N = 1000000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  int16_t l;
+  int8_t i;
+  uint8_t s:7;
+  bool b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
