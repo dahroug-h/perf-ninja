53ed3ba57d7ede5617be948e4502860d773849eb
Anthony Van Herrewege <anthony@anthonyvh.com>
2025-11-08 17:19:02 +0100

    loop_tiling_1 - Reduce tile size even more

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -2,19 +2,18 @@
 #include <algorithm>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  constexpr int I_TILE_SIZE = 32;
-  constexpr int J_TILE_SIZE = 64;
+  constexpr int I_TILE_SIZE = 16;
+  constexpr int J_TILE_SIZE = 16;
 
   int size = in.size();
 
-  for (int j_block = 0; j_block < size; j_block += J_TILE_SIZE) {
-    const int j_max = std::min(j_block + J_TILE_SIZE, size);
+  for (int i_block = 0; i_block < size; i_block += I_TILE_SIZE) {
+    const int i_max = std::min(i_block + I_TILE_SIZE, size);
+    for (int j_block = 0; j_block < size; j_block += J_TILE_SIZE) {
+      const int j_max = std::min(j_block + J_TILE_SIZE, size);
 
-    for (int i_block = 0; i_block < size; i_block += I_TILE_SIZE) {
-      const int i_max = std::min(i_block + I_TILE_SIZE, size);
-
-      for (int j = j_block; j < j_max; ++j) {
-        for (int i = i_block; i < i_max; ++i) {
+      for (int i = i_block; i < i_max; ++i) {
+        for (int j = j_block; j < j_max; ++j) {
           out[i][j] = in[j][i];
         }
       }
