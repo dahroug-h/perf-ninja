342068d0dc98a981ddad702a1cb940eeff0d1f4a
Anthony Van Herrewege <anthony@anthonyvh.com>
2025-11-08 17:17:41 +0100

    loop_tiling_1 - Tile size adjustments

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -2,19 +2,20 @@
 #include <algorithm>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  constexpr int TILE_SIZE = 64;
+  constexpr int I_TILE_SIZE = 32;
+  constexpr int J_TILE_SIZE = 64;
 
   int size = in.size();
-  for (int i_block = 0; i_block < size; i_block += TILE_SIZE) {
-    const int i_offset_max = std::min(TILE_SIZE, size - i_block);
 
-    for (int j_block = 0; j_block < size; j_block += TILE_SIZE) {
-      const int j_offset_max = std::min(TILE_SIZE, size - j_block);
+  for (int j_block = 0; j_block < size; j_block += J_TILE_SIZE) {
+    const int j_max = std::min(j_block + J_TILE_SIZE, size);
 
-      for (int i_offset = 0; i_offset < i_offset_max; ++i_offset) {
-        for (int j_offset = 0; j_offset < j_offset_max; ++j_offset) {
-          out[i_block + i_offset][j_block + j_offset] =
-              in[j_block + j_offset][i_block + i_offset];
+    for (int i_block = 0; i_block < size; i_block += I_TILE_SIZE) {
+      const int i_max = std::min(i_block + I_TILE_SIZE, size);
+
+      for (int j = j_block; j < j_max; ++j) {
+        for (int i = i_block; i < i_max; ++i) {
+          out[i][j] = in[j][i];
         }
       }
     }
