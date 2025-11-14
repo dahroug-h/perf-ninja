5fafa11f26c750a734b486704c54eb1b4d187914
Peter Moroz <ptr.moroz@gmail.com>
2025-11-14 21:17:22 +0300

    add: solution of virtual call mispredict lab

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -6,15 +6,27 @@ void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    InstanceArray tmpA, tmpB, tmpC;
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
+            tmpA.push_back(std::make_unique<ClassA>());
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
+            tmpB.push_back(std::make_unique<ClassB>());
         } else {
-            array.push_back(std::make_unique<ClassC>());
+            tmpC.push_back(std::make_unique<ClassC>());
         }
+    }
+
+    array.reserve(tmpA.size() + tmpB.size() + tmpC.size());
+    for (size_t i = 0; i < tmpA.size(); i++) {
+        array.push_back(std::move(tmpA[i]));
+    }
+    for (size_t i = 0; i < tmpB.size(); i++) {
+        array.push_back(std::move(tmpB[i]));
+    }
+    for (size_t i = 0; i < tmpC.size(); i++) {
+        array.push_back(std::move(tmpC[i]));
     }
 }
 
