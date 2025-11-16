58bba7e0700fd2a0467cd7f21a3784618b505105
Peter Moroz <ptr.moroz@gmail.com>
2025-11-16 16:37:09 +0300

    put LUT directly into body of histogram function

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -2,7 +2,33 @@
 
 #include <cstdint>
 
-static std::size_t mapToBucket(std::size_t v) {
+// static std::size_t mapToBucket(std::size_t v) {
+//   static std::size_t lut[101] = {
+//     // 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17
+//        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+//        1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+//        2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+//        3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,
+//        4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,
+//        5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,
+//        6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,
+//        DEFAULT_BUCKET
+//   };
+//   const size_t idx = __builtin_unpredictable(v < 101) ? v : 100;
+//   return lut[idx];
+// }
+
+// std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
+//   std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+//   for (auto v : values) {
+//     retBuckets[mapToBucket(v)]++;
+//   }
+//   return retBuckets;
+// }
+
+std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
+  std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+
   static std::size_t lut[101] = {
     // 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17
        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -13,15 +39,11 @@ static std::size_t mapToBucket(std::size_t v) {
        5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,
        6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,
        DEFAULT_BUCKET
-  };
-  const size_t idx = __builtin_unpredictable(v < 101) ? v : 100;
-  return lut[idx];
-}
+  };  
 
-std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
-  std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
   for (auto v : values) {
-    retBuckets[mapToBucket(v)]++;
+    const size_t idx = __builtin_unpredictable(v < 101) ? v : 100;
+    retBuckets[lut[idx]]++;
   }
   return retBuckets;
 }
