3c777c5e987123648ec2797d52477cd6e013bfe2
Peter Moroz <ptr.moroz@gmail.com>
2025-11-16 21:32:33 +0300

    add: solution of conditional store lab

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -6,9 +6,10 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
   for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
-    }
+    const bool inbounds = (lower <= item.first) && (item.first <= upper);
+    const auto curr = output[count];
+    output[count] = __builtin_unpredictable(inbounds) ? item : curr;
+    count = __builtin_unpredictable(inbounds) ? (count + 1) : count;    
   }
   return count;
 }
