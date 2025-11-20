39189b4c655cd2634229748c42b7e9029327ebe3
nnjankovic <shone23jankovic@gmail.com>
2025-11-20 23:45:00 +0100

    core_bound_function_inline

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,6 +3,35 @@
 #include <algorithm>
 #include <stdlib.h>
 
+#ifdef SOULUTION 
+
+[[gnu::always_inline]] int compare(const void *lhs, const void *rhs) {
+  auto &a = *reinterpret_cast<const S *>(lhs);
+  auto &b = *reinterpret_cast<const S *>(rhs);
+
+  if (a.key1 < b.key1)
+    return -1;
+
+  if (a.key1 > b.key1)
+    return 1;
+
+  if (a.key2 < b.key2)
+    return -1;
+
+  if (a.key2 > b.key2)
+    return 1;
+
+  return 0;
+}
+
+
+
+void solution(std::array<S, N> &arr) {
+  std::sort(arr.begin(), arr.end(), compare);
+}
+
+#else
+
 static int compare(const void *lhs, const void *rhs) {
   auto &a = *reinterpret_cast<const S *>(lhs);
   auto &b = *reinterpret_cast<const S *>(rhs);
@@ -22,6 +51,9 @@ static int compare(const void *lhs, const void *rhs) {
   return 0;
 }
 
+
 void solution(std::array<S, N> &arr) {
   qsort(arr.data(), arr.size(), sizeof(S), compare);
 }
+
+#endif
