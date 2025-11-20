c968d5eb3efb0eea01c37e7a506ca72a01bc35c3
nnjankovic <shone23jankovic@gmail.com>
2025-11-21 00:34:41 +0100

    core_bound_function_inline3

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <stdlib.h>
 
-#ifdef SOLUTION
+//#ifdef SOLUTION
 
 [[gnu::always_inline]] bool compare(const S& a, const S& b) {
   if (a.key1 < b.key1)
@@ -27,30 +27,30 @@ void solution(std::array<S, N> &arr) {
   std::sort(arr.begin(), arr.end(), compare);
 }
 
-#else
+// #else
 
-static int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
+// static int compare(const void *lhs, const void *rhs) {
+//   auto &a = *reinterpret_cast<const S *>(lhs);
+//   auto &b = *reinterpret_cast<const S *>(rhs);
 
-  if (a.key1 < b.key1)
-    return -1;
+//   if (a.key1 < b.key1)
+//     return -1;
 
-  if (a.key1 > b.key1)
-    return 1;
+//   if (a.key1 > b.key1)
+//     return 1;
 
-  if (a.key2 < b.key2)
-    return -1;
+//   if (a.key2 < b.key2)
+//     return -1;
 
-  if (a.key2 > b.key2)
-    return 1;
+//   if (a.key2 > b.key2)
+//     return 1;
 
-  return 0;
-}
+//   return 0;
+// }
 
 
-void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
-}
+// void solution(std::array<S, N> &arr) {
+//   qsort(arr.data(), arr.size(), sizeof(S), compare);
+// }
 
-#endif
+// #endif
