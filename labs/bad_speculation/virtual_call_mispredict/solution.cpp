06e658845513d7c720afcb5a30841cb693881b66
YukunJ <yukunj.cs@gmail.com>
2025-11-21 03:04:55 +0000

    virtual call

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -6,16 +6,20 @@ void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    InstanceArray array1, array2, array3;
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
+            array1.push_back(std::make_unique<ClassA>());
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
+            array2.push_back(std::make_unique<ClassB>());
         } else {
-            array.push_back(std::make_unique<ClassC>());
+            array3.push_back(std::make_unique<ClassC>());
         }
     }
+    array.insert(array.end(), std::make_move_iterator(array1.begin()), std::make_move_iterator(array1.end()));
+    array.insert(array.end(), std::make_move_iterator(array2.begin()), std::make_move_iterator(array2.end()));
+    array.insert(array.end(), std::make_move_iterator(array3.begin()), std::make_move_iterator(array3.end()));
 }
 
 // Invoke the `handle` method on all instances in `output`
