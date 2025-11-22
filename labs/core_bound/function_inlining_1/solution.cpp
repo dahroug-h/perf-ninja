bd3c20851b611db71ed8f782296dcd3cacc1ed8d
nnjankovic <shone23jankovic@gmail.com>
2025-11-22 12:56:46 +0100

    function_inlining

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -5,20 +5,8 @@
 
 //#ifdef SOLUTION
 
-[[gnu::always_inline]] bool compare(const S& a, const S& b) {
-  if (a.key1 < b.key1)
-    return true;
-
-  if (a.key1 > b.key1)
-    return false;
-
-  if (a.key2 < b.key2)
-    return true;
-
-  if (a.key2 > b.key2)
-    return false;
-
-  return true;
+[[gnu::always_inline]] bool compare(const S& a, const S& b) noexcept {
+  return a.key1 < b.key1 || (a.key1 == b.key1 && a.key2 < b.key2);
 }
 
 
