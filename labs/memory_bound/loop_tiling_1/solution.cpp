095e5a6147df3c7fe7028c7702e2e8de1a9db21f
hanyuhang <18700873675@163.com>
2025-11-23 13:20:01 +0800

    test for Loop Tiling

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,6 +1,25 @@
 #include "solution.hpp"
 #include <algorithm>
 
+#define SOLUTION
+
+#ifdef SOLUTION
+bool solution(MatrixOfDoubles& in, MatrixOfDoubles& out) {
+  static constexpr int TILE_SIZE = 16;
+  int size = in.size();
+  for (int ii = 0; ii < size; ii+= TILE_SIZE) {
+    for (int jj = 0; jj < size; jj+= TILE_SIZE) {
+      for (int i = ii; i < std::min(ii + TILE_SIZE, size); i++) {
+        for (int j = jj; j < std::min(jj + TILE_SIZE, size); j++) {
+          out[i][j] = in[j][i];
+        }
+      }
+    }
+  }
+
+  return out[0][size - 1];
+}
+#else
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
   for (int i = 0; i < size; i++) {
@@ -10,3 +29,4 @@ bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   }
   return out[0][size - 1];
 }
+#endif
