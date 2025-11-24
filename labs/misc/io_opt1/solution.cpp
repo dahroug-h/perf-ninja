a8e4aaa42b9ac7fdac03c1b143df3c224bab012b
YukunJ <yukunj.cs@gmail.com>
2025-11-24 02:37:11 +0000

    mapped file for io

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -1,22 +1,26 @@
 #include "solution.hpp"
-
+#include "MappedFile.hpp"
 #include <fstream>
 #include <stdexcept>
 
 uint32_t solution(const char *file_name) {
-  std::fstream file_stream{file_name};
-  if (!file_stream.is_open())
-    throw std::runtime_error{"The file could not be opened"};
-
+  // std::fstream file_stream{file_name};
+  // if (!file_stream.is_open())
+  //   throw std::runtime_error{"The file could not be opened"};
+  MappedFile mapped_file{file_name};
+  std::string_view file_view = mapped_file.getContents();
   // Initial value has all bits set to 1
   uint32_t crc = 0xff'ff'ff'ff;
 
   // Update the CRC32 value character by character
-  char c;
-  while (true) {
-    file_stream.read(&c, 1);
-    if (file_stream.eof())
-      break;
+  // char c;
+  // while (true) {
+  //   file_stream.read(&c, 1);
+  //   if (file_stream.eof())
+  //     break;
+  //   update_crc32(crc, static_cast<uint8_t>(c));
+  // }
+  for (char c: file_view) {
     update_crc32(crc, static_cast<uint8_t>(c));
   }
 
