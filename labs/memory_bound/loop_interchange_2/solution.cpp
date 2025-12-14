f7053d9ab8a550cd95b72357f0e16c72ca7a9619
Hanchen Liu <39453889+HCPhy@users.noreply.github.com>
2025-12-14 00:34:01 -0500

    fixed vector

--- labs/memory_bound/loop_interchange_2/solution.cpp
+++ labs/memory_bound/loop_interchange_2/solution.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <fstream>
 #include <ios>
+#include <vector>
 
 // Applies Gaussian blur in independent vertical lines
 static void filterVertically(uint8_t *output, const uint8_t *input,
