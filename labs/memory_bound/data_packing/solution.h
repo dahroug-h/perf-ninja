0cc2bcbd12b812ef886e88dd58b10685c320542f
thomas kim <kimthomas109@gmail.com>
2026-01-08 04:37:18 +0000

    data_packing attempt 1 - reordered fields and reduced size of some fields

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,10 +7,10 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
+  float d;
+  short l;
+  int i = 8;
+  short s = 8;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
