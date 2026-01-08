717c555f7bfd88c63beb97f3bc7d935d52d36656
thomas kim <kimthomas109@gmail.com>
2026-01-08 04:50:19 +0000

    use denis' solution

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,13 +8,19 @@ constexpr int maxRandom = 100;
 // FIXME: this data structure can be reduced in size
 struct S {
   float d;
-  short l;
-  int i = 8;
-  short s = 8;
-  bool b;
+  unsigned long long l: 16;
+  unsigned int i: 8;
+  unsigned short s: 7;
+  bool b: 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+template <int N>
+class TD;
+
+// never compiles but shows the value of sizeof(S)
+// TD<sizeof(S)> td;
 
 void init(std::vector<S> &arr);
 S create_entry(int first_value, int second_value);
