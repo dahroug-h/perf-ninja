cc81f2ae1c1eaf3f11a6d474d04d60fb35436c91
Alexandr Stepanov <stal76@yandex.ru>
2026-01-08 18:07:39 +0300

    Solved Compiler Intrinsics 1 lab

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,5 +1,6 @@
 #include "solution.hpp"
 #include <iostream>
+#include <immintrin.h>
 
 // Find the longest line in a file.
 // Implementation uses ternary operator with a hope that compiler will
@@ -12,7 +13,7 @@ if (s == '\n') {
 } else {
   curLineLength++;
 }*/
-unsigned solution(const std::string &inputContents) {
+unsigned solution1(const std::string &inputContents) {
   unsigned longestLine = 0;
   unsigned curLineLength = 0;
 
@@ -22,4 +23,42 @@ unsigned solution(const std::string &inputContents) {
   }
 
   return longestLine;
+}
+
+unsigned solution2(const std::string &inputContents) {
+  uint32_t longestLine = 0;
+  uint32_t curLineLength = 0;
+  __m256i slashn = _mm256_set1_epi8('\n');
+  const char* buffer = inputContents.data();
+  uint32_t N = inputContents.size();
+  uint32_t i = 0;
+
+  while (i + 32 < N) {
+    uint32_t diff = _mm256_movemask_epi8(_mm256_cmpeq_epi8(_mm256_loadu_si256((const __m256i*)buffer), slashn));
+    i += 32;
+    buffer += 32;
+    uint32_t blockLen = 32;
+    
+    while (diff != 0) {
+      uint32_t pos = _tzcnt_u32(diff);
+      longestLine = std::max(curLineLength + pos, longestLine);
+      curLineLength = 0;
+      blockLen -= (pos + 1);
+      diff = (pos == 31 ? 0 : diff >> (pos + 1));
+    }
+    curLineLength += blockLen;
+  }
+  longestLine = std::max(curLineLength, longestLine);
+
+  N -= i;
+  for (i = 0; i < N; i++) {
+    curLineLength = (buffer[i] == '\n') ? 0 : curLineLength + 1;
+    longestLine = std::max(curLineLength, longestLine);
+  }
+
+  return longestLine;
+}
+
+unsigned solution(const std::string &inputContents) {
+  return solution2(inputContents);
 }
