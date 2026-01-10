8cfea5da700e2c9cbbb9b91e77c83a35bc7d0a5f
ruitianzhong <zhongruitian2003@qq.com>
2026-01-10 06:56:05 +0000

    conditional store

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -1,14 +1,20 @@
-
 #include "solution.h"
-
-// Select items which have S.first in range [lower..upper]
-std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
+#define SOLUTION
+//  Select items which have S.first in range [lower..upper]
+std::size_t select(std::array<S, N>& output, const std::array<S, N>& input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
   for (const auto item : input) {
+#ifdef SOLUTION
+    bool should_store = (lower <= item.first) && (item.first <= upper);
+    output[count] = item;
+    count += should_store * 1;
+#else
     if ((lower <= item.first) && (item.first <= upper)) {
       output[count++] = item;
     }
+
+#endif
   }
   return count;
 }
