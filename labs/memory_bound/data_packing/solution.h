f066a76da77df0b2214ee1fe906ef43e457aec2b
ruitianzhong <zhongruitian2003@qq.com>
2026-01-10 09:29:50 +0000

    finish data packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,11 +7,13 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
+  short s;
+  bool b;
+
   int i;
   long long l;
-  short s;
+
   double d;
-  bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
