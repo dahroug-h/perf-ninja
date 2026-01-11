569d65711face3f9730384ac74259eada2ea5ce5
ruitianzhong <zhongruitian2003@qq.com>
2026-01-11 16:22:50 +0000

    update dep_chain1

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -4,7 +4,7 @@
 
 #include <array>
 #include <iostream>
-// update dep_chain
+
 unsigned getSumOfDigits(unsigned n) {
   unsigned sum = 0;
   while (n != 0) {
@@ -13,23 +13,6 @@ unsigned getSumOfDigits(unsigned n) {
   }
   return sum;
 }
-
-#define DO()                                              \
-  do {                                                    \
-    if (p[i] != nullptr) {                                \
-      processed++;                                        \
-      if (p[i]->value == v) {                             \
-        retVal += getSumOfDigits(v);                      \
-      }                                                   \
-                                                          \
-      if (i + 1 < num_partitions &&      \
-                           heads[i + 1] == p[i]->next) { \
-        p[i] = nullptr;                                   \
-      } else {                                            \
-        p[i] = p[i]->next;                                \
-      }                                                   \
-    }                                                     \
-  } while (0)
 
   // Task: lookup all the values from l2 in l1.
   // For every found value, find the sum of its digits.
@@ -41,265 +24,56 @@ unsigned getSumOfDigits(unsigned n) {
   //       to get the node N+1 you need to retrieve the node N first.
   //       Think how you can execute multiple dependency chains in parallel.
 
-  constexpr int num_partitions = 8;
-// unsigned solution(List* l1, List* l2) {
-//   unsigned retVal = 0;
-
-//   List* head2 = l2;
-
-//   List *fast = l2, *slow = l2;
-
-//   while (fast != nullptr && fast->next != nullptr) {
-//     slow = slow->next;
-//     fast = fast->next->next;
-//   }
-
-//   List* mid = slow;
-
-//   if (false) {
-//     // O(N^2) algorithm:
-//     while (l1) {
-//       unsigned v = l1->value;
-//       l2 = head2;
-//       while (l2) {
-//         if (l2->value == v) {
-//           retVal += getSumOfDigits(v);
-//           break;
-//         }
-//         l2 = l2->next;
-//       }
-//       l1 = l1->next;
-//     }
-//   } else {
-//     while (l1) {
-//       unsigned v = l1->value;
-//       l2 = head2;
-
-//       auto p1 = head2;
-//       auto p2 = mid;
-
-//       while (p1 != mid && p2 != nullptr) {
-//         if (p1->value == v) {
-//           retVal += getSumOfDigits(p1->value);
-//         }
-//         if (p2->value == v) {
-//           retVal += getSumOfDigits(p2->value);
-//         }
-
-//         p2 = p2->next;
-//         p1 = p1->next;
-//       }
-
-//       l1 = l1->next;
-//     }
-//   }
-//   return retVal;
-// }
-
+  constexpr int num_partitions = 32;
+  constexpr int num_cached = 4;
 #ifndef SOLUTION
-unsigned solution(List* l1, List* l2) {
-  unsigned retVal = 0;
+  unsigned solution(List* l1, List* l2) {
+    unsigned retVal = 0;
 
-  List* head2 = l2;
-  while (l1) {
-    unsigned v = l1->value;
-    l2 = head2;
-    while (l2) {
-      if (l2->value == v) {
-        retVal += getSumOfDigits(v);
-        break;
+    List* head2 = l2;
+    while (l1) {
+      unsigned v = l1->value;
+      l2 = head2;
+      while (l2) {
+        if (l2->value == v) {
+          retVal += getSumOfDigits(v);
+          break;
+        }
+        l2 = l2->next;
       }
-      l2 = l2->next;
+      l1 = l1->next;
     }
-    l1 = l1->next;
-  }
 
-  return retVal;
-}
+    return retVal;
+  }
 #else
-#ifdef MULTI_OVERLAP
-unsigned solution(List* l1, List* l2) {
-  // printf("overlap solution\n");
-  unsigned retVal = 0;
 
-  List* head2 = l2;
+  unsigned solution(List* l1, List* l2) {
+    unsigned retVal = 0;
+    unsigned cache[num_cached];
+    List* head2 = l2;
+    while (l1) {
+      int cnt = 0;
+      while (l1 && cnt < num_cached) {
+        cache[cnt] = l1->value;
+        cnt++;
+        l1 = l1->next;
+      }
 
-  List* p[num_partitions];
-  List* heads[num_partitions];
-  int l2_len = 0;
-  l2 = head2;
-  while (l2) {
-    l2_len++;
-    l2 = l2->next;
-  }
-
-  int partition_cnt = l2_len / num_partitions;
-  l2 = head2;
-  for (int i = 0; i < l2_len; i++) {
-    if (i % partition_cnt == 0) {
-      heads[i / partition_cnt] = l2;
-    }
-    l2 = l2->next;
-  }
-
-  while (l1) {
-    unsigned v = l1->value;
-    l2 = head2;
-    int processed = 0;
-
-    for (int i = 0; i < num_partitions; i++) {
-      p[i] = heads[i];
-    }
-    while (true) {
-      
-      int i;
-      #pragma unroll 8
-      for ( i = 0; i < num_partitions; i++) {
-        if (p[i] != nullptr) {
-          processed++;
-
-          if (p[i]->value == v) {
-            retVal += getSumOfDigits(v);
-          }
-
-          if (i + 1 < num_partitions && heads[i + 1] == p[i]->next) {
-            p[i] = nullptr;
-          } else {
-            p[i] = p[i]->next;
+      l2 = head2;
+      while (l2) {
+        for (int i = 0; i < cnt; i++) {
+          if (l2->value == cache[i]) {
+            retVal += getSumOfDigits(cache[i]);
+            break;
           }
         }
+        l2 = l2->next;
       }
-
-      // int i;
-      // i=0;DO();
-      // i=1;DO();
-      // i=2;DO();
-      // i=3;DO();
-      // i=4;DO();
-      // i=5;DO();
-      // i=6;DO();
-      // i=7;DO();
-
-      if (processed == l2_len) {
-        break;
-      }
+     
     }
 
-    l1 = l1->next;
+    return retVal;
   }
-
-  return retVal;
-}
-
-
-// unsigned solution(List* l1, List* l2) {
-//   unsigned retVal = 0;
-
-//   List* head2 = l2;
-
-//   List* p[num_partitions];
-//   List* heads[num_partitions];
-//   int l2_len = 0;
-//   l2 = head2;
-//   while (l2) {
-//     l2_len++;
-//     l2 = l2->next;
-//   }
-
-//   int partition_cnt = l2_len / num_partitions;
-//   l2 = head2;
-//   for (int i = 0; i < l2_len; i++) {
-//     if (i % partition_cnt == 0) {
-//       heads[i / partition_cnt] = l2;
-//     }
-//     l2 = l2->next;
-//   }
-
-//   while (l1) {
-//     unsigned v = l1->value;
-//     l2 = head2;
-//     auto exit = true;
-
-//     for (int i = 0; i < num_partitions; i++) {
-//       p[i] = heads[i];
-//     }
-//     do {
-//       exit = true;
-//       for (int i = 0; i < num_partitions; i++) {
-//         if (p[i] != nullptr) {
-//           exit = false;
-
-//           if (p[i]->value == v) {
-//             retVal += getSumOfDigits(v);
-//           }
-
-//           if (i + 1 < num_partitions && heads[i + 1] == p[i]->next) {
-//             p[i] = nullptr;
-//           } else {
-//             p[i] = p[i]->next;
-//           }
-//         }
-//       }
-//     } while (!exit);
-
-//     l1 = l1->next;
-//   }
-
-//   return retVal;
-// }
-#else
-unsigned solution(List *l1, List *l2) {
-  unsigned retVal = 0;
-
-  List *head2 = l2;
-
-  List *fast = l2, *slow = l2;
-
-  while (fast != nullptr && fast->next != nullptr) {
-    slow = slow->next;
-    fast = fast->next->next;
-  }
-
-  List* mid = slow;
-
-  // O(N^2) algorithm:
-  // while (l1) {
-  //   unsigned v = l1->value;
-  //   l2 = head2;
-  //   while (l2) {
-  //     if (l2->value == v) {
-  //       retVal += getSumOfDigits(v);
-  //       break;
-  //     }
-  //     l2 = l2->next;
-  //   }
-  //   l1 = l1->next;
-  // }
-
-  while (l1) {
-    unsigned v = l1->value;
-    l2 = head2;
-
-    auto p1 = head2;
-    auto p2 = mid;
-
-    while (p1 != mid && p2 != nullptr) {
-      if (p1->value == v) {
-        retVal += getSumOfDigits(p1->value);
-      }
-      if (p2->value == v) {
-        retVal += getSumOfDigits(p2->value);
-      }
-
-      p2 = p2->next;
-      p1 = p1->next;
-    }
-
-    l1 = l1->next;
-  }
-  return retVal;
-}
-
-#endif
 
 #endif
