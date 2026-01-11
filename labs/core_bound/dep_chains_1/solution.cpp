ad7f0d204e0f5ebbbe45a4690266fc7fa5ce941d
ruitianzhong <zhongruitian2003@qq.com>
2026-01-11 07:59:39 +0000

    optimize dep_chain

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -1,5 +1,5 @@
 #include "solution.hpp"
-#define SOLUTION
+// #define SOLUTION
 #define MULTI_OVERLAP
 
 #include <array>
@@ -14,17 +14,34 @@ unsigned getSumOfDigits(unsigned n) {
   return sum;
 }
 
-// Task: lookup all the values from l2 in l1.
-// For every found value, find the sum of its digits.
-// Return the sum of all digits in every found number.
-// Both lists have no duplicates and elements placed in *random* order.
-// Do NOT sort any of the lists. Do NOT store elements in a hash_map/sets.
+#define DO()                                              \
+  do {                                                    \
+    if (p[i] != nullptr) {                                \
+      processed++;                                        \
+      if (p[i]->value == v) {                             \
+        retVal += getSumOfDigits(v);                      \
+      }                                                   \
+                                                          \
+      if (__glibc_unlikely(i + 1 < num_partitions &&      \
+                           heads[i + 1] == p[i]->next)) { \
+        p[i] = nullptr;                                   \
+      } else {                                            \
+        p[i] = p[i]->next;                                \
+      }                                                   \
+    }                                                     \
+  } while (0)
 
-// Hint: Traversing a linked list is a long data dependency chain:
-//       to get the node N+1 you need to retrieve the node N first.
-//       Think how you can execute multiple dependency chains in parallel.
+  // Task: lookup all the values from l2 in l1.
+  // For every found value, find the sum of its digits.
+  // Return the sum of all digits in every found number.
+  // Both lists have no duplicates and elements placed in *random* order.
+  // Do NOT sort any of the lists. Do NOT store elements in a hash_map/sets.
 
-constexpr int num_partitions = 8;
+  // Hint: Traversing a linked list is a long data dependency chain:
+  //       to get the node N+1 you need to retrieve the node N first.
+  //       Think how you can execute multiple dependency chains in parallel.
+
+  constexpr int num_partitions = 8;
 // unsigned solution(List* l1, List* l2) {
 //   unsigned retVal = 0;
 
@@ -102,6 +119,7 @@ unsigned solution(List* l1, List* l2) {
 #else
 #ifdef MULTI_OVERLAP
 unsigned solution(List* l1, List* l2) {
+  // printf("overlap solution\n");
   unsigned retVal = 0;
 
   List* head2 = l2;
@@ -127,29 +145,46 @@ unsigned solution(List* l1, List* l2) {
   while (l1) {
     unsigned v = l1->value;
     l2 = head2;
-    auto exit = true;
+    int processed = 0;
 
     for (int i = 0; i < num_partitions; i++) {
       p[i] = heads[i];
     }
-    do {
-      exit = true;
-      for (int i = 0; i < num_partitions; i++) {
-        if (p[i] != nullptr) {
-          exit = false;
+    while (true) {
+      
+      // int i;
+      // #pragma unroll 8
+      // for ( i = 0; i < num_partitions; i++) {
+      //   if (p[i] != nullptr) {
+      //     processed++;
 
-          if (p[i]->value == v) {
-            retVal += getSumOfDigits(v);
-          }
+      //     if (p[i]->value == v) {
+      //       retVal += getSumOfDigits(v);
+      //     }
 
-          if (i + 1 < num_partitions && heads[i + 1] == p[i]->next) {
-            p[i] = nullptr;
-          } else {
-            p[i] = p[i]->next;
-          }
-        }
+      //     if (__glibc_unlikely(i + 1 < num_partitions &&
+      //                          heads[i + 1] == p[i]->next)) {
+      //       p[i] = nullptr;
+      //     } else {
+      //       p[i] = p[i]->next;
+      //     }
+      //   }
+      // }
+
+      int i;
+      i=0;DO();
+      i=1;DO();
+      i=2;DO();
+      i=3;DO();
+      i=4;DO();
+      i=5;DO();
+      i=6;DO();
+      i=7;DO();
+
+      if (processed == l2_len) {
+        break;
       }
-    } while (!exit);
+    }
 
     l1 = l1->next;
   }
@@ -157,6 +192,62 @@ unsigned solution(List* l1, List* l2) {
   return retVal;
 }
 
+
+// unsigned solution(List* l1, List* l2) {
+//   unsigned retVal = 0;
+
+//   List* head2 = l2;
+
+//   List* p[num_partitions];
+//   List* heads[num_partitions];
+//   int l2_len = 0;
+//   l2 = head2;
+//   while (l2) {
+//     l2_len++;
+//     l2 = l2->next;
+//   }
+
+//   int partition_cnt = l2_len / num_partitions;
+//   l2 = head2;
+//   for (int i = 0; i < l2_len; i++) {
+//     if (i % partition_cnt == 0) {
+//       heads[i / partition_cnt] = l2;
+//     }
+//     l2 = l2->next;
+//   }
+
+//   while (l1) {
+//     unsigned v = l1->value;
+//     l2 = head2;
+//     auto exit = true;
+
+//     for (int i = 0; i < num_partitions; i++) {
+//       p[i] = heads[i];
+//     }
+//     do {
+//       exit = true;
+//       for (int i = 0; i < num_partitions; i++) {
+//         if (p[i] != nullptr) {
+//           exit = false;
+
+//           if (p[i]->value == v) {
+//             retVal += getSumOfDigits(v);
+//           }
+
+//           if (i + 1 < num_partitions && heads[i + 1] == p[i]->next) {
+//             p[i] = nullptr;
+//           } else {
+//             p[i] = p[i]->next;
+//           }
+//         }
+//       }
+//     } while (!exit);
+
+//     l1 = l1->next;
+//   }
+
+//   return retVal;
+// }
 #else
 unsigned solution(List *l1, List *l2) {
   unsigned retVal = 0;
