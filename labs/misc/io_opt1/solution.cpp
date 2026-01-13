28f04918b64f04bc9dddff94001f5dc91ca5179e
ruitianzhong <zhongruitian2003@qq.com>
2026-01-13 14:49:14 +0000

    io optimization

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -1,26 +1,25 @@
 #include "solution.hpp"
 
 #include <fstream>
-#include <stdexcept>
 
-uint32_t solution(const char *file_name) {
-  std::fstream file_stream{file_name};
-  if (!file_stream.is_open())
-    throw std::runtime_error{"The file could not be opened"};
+#include "MappedFile.hpp"
+#include <stdexcept
+
+uint32_t solution(const char* file_name) {
+  MappedFile file{file_name};
 
   // Initial value has all bits set to 1
   uint32_t crc = 0xff'ff'ff'ff;
 
   // Update the CRC32 value character by character
   char c;
-  while (true) {
-    file_stream.read(&c, 1);
-    if (file_stream.eof())
-      break;
+
+  auto content = file.getContents();
+
+  for (char c : content) {
     update_crc32(crc, static_cast<uint8_t>(c));
   }
 
-  // Invert the bits
   crc ^= 0xff'ff'ff'ff;
 
   return crc;
